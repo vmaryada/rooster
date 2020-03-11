8a47cd24b5800020ef714af7b7bc79066a15094f
Vineeth Reddy Maryada <vmaryada@unomaha.edu>
2020-03-11 09:48:50 -0500

    Updates

---
+++ precache-manifest.661ff3e1f1b5a7cfc1c549ad91eccefb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121dfcb06ec8acf6187f80c50b6ee783",
+    "url": "/rooster/index.html"
+  },
+  {
+    "revision": "28e9a3c0a14f55b1474b",
+    "url": "/rooster/static/css/main.2c85bb11.chunk.css"
+  },
+  {
+    "revision": "e2147713ad082ef54163",
+    "url": "/rooster/static/js/2.a57dd2e6.chunk.js"
+  },
+  {
+    "revision": "3d416322b0ec4a5a91d5be4afe7c848c",
+    "url": "/rooster/static/js/2.a57dd2e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28e9a3c0a14f55b1474b",
+    "url": "/rooster/static/js/main.9d5d8b30.chunk.js"
+  },
+  {
+    "revision": "1f2bb8f6fde4c47a05aa",
+    "url": "/rooster/static/js/runtime-main.11466a17.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/rooster/static/media/default-image.5a021ab9.png"
+  },
+  {
+    "revision": "166758d03358c07992f2e45d6ac05dab",
+    "url": "/rooster/static/media/rooster.166758d0.png"
+  }
+]);
